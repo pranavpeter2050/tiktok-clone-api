8c16a2585135a0db1c7b272a7bbdf119a04bcbae
pranav2050 <97501797+pranavpeter2050@users.noreply.github.com>
2024-05-17 08:43:56 +0530

    get('/user') api route changed to get('/logged-in-user')

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/logged-in-user', [UserController::class, 'loggedInUser']);
 });
